a10f17917752b8501dabc054afd670b0d19bc532
NadiaEs829 <nadia_amores@hotmail.com>
2024-10-22 17:35:32 +0000

    working

--- MyLLTester.java
+++ MyLLTester.java
@@ -1,3 +1,4 @@
+//Nadia Escamilla, pd: 7, date: 10/22/24
 public class MyLLTester.java
 {
   public static void main(String [] args)
--- MyLinkedList.java
+++ MyLinkedList.java
@@ -1,5 +1,7 @@
+//Nadia Escamilla, pd: 7, date: 10/22/24
 public class MyLinkedList
 {
 
+private ListNode head; 
 
-}
+}//end class
