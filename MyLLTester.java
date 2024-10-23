9bb1e55374bd90e07145af0628c4f2ce551b1ec9
NadiaEs829 <nadia_amores@hotmail.com>
2024-10-23 17:37:41 +0000

    working - end of 10/23

--- MyLLTester.java
+++ MyLLTester.java
@@ -3,8 +3,15 @@ public class MyLLTester.java
 {
   public static void main(String [] args)
   {
-    
-
+  //Day 1: Create a list, add a few values to it, test toString
+  //Day 2: Test get, set, and toString
+  //Day 3: Test all versions of remove
+  //Day 4: Final edits, make sure you check special cases - all add versions on an empty list, all remove versions on an empty list
+  private ListNode head;
+  head.add("Hello");
+  head.add("Hola");
+  head.add("Hi");
+  head.add();
   } // end main
   
 } // end class
--- MyLinkedList.java
+++ MyLinkedList.java
@@ -1,7 +1,44 @@
 //Nadia Escamilla, pd: 7, date: 10/22/24
 public class MyLinkedList
 {
+//Day 1: Add fields, create constructor, create at least 1 add method, write toString
+//Day 2: Add additional constructors, set and get methods
+//Day 3: Add remove methods
+//Day 4: debug
 
-private ListNode head; 
+//fields
+private ListNode head;
+int size;
+//Constructor
+public MyLinkedList(){
+    head = null;
+    int size = 0;
+}//end constructor
+
+///add method
+public boolean add(Object myObj){
+    head.setNext(myObj);
+}//end add method 1
+
+//set method
+public Object set(int i, Object myObj){
+
+}//end set method
+
+//get method
+public Object get(int i){
+    return //__.indexOf(i);
+}//end get method
+
+//remove methods
+public Object remove(int i){
+
+}//end remove method
+
+
+//toString method
+public String toString(){
+    return ;
+}//end toString method
 
 }//end class
